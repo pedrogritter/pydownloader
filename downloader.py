4b47f7c56420c9556b6b17e8ad3c4bec26dfa003
Pedro Gritter <pedro12345g@gmail.com>
2019-08-17 23:15:32 +0100

    Audio download is now able to convert webm -> mp3 on request - Using pydub

--- downloader.py
+++ downloader.py
@@ -5,22 +5,30 @@ __version__ = "1.0"
 
 from pytube import YouTube
 from pytube import Playlist
+from pydub import AudioSegment
 import argparse, time
+#from general import append_to_file
+
 
 #### Functions ###
 
 def choose_download(youtube):
     streamFormat = int(input("Insert the itag number: "))
     stream = youtube.streams.get_by_itag(streamFormat)
+    filesize = stream.filesize
+
 
     if args.verbose:
-        print('Download started... ')
         print(" ")
-        print("Download details:")
-        print("Name:"+ str(stream.default_filename))
-        print("Size:" + str(stream.filesize) + " bytes")
+        print('Download started... \n')
+        print(" ")
+        print("Download details:\n")
+        print("Name:"+ str(stream.default_filename)+"\n")
+        print("Size:" + str(round(stream.filesize/(1024*1024),2)) + 'MB\n')
         print(" ")
         print("Wait...")
+        print(" ")
+
 
     dl_start = time.time()
     stream.download()
@@ -30,8 +38,18 @@ def choose_download(youtube):
     print("Download Finished!")
 
     if args.verbose:
-        print("In: " + str(dl_totaltime) + "s")
+        print("In: " + str(round(dl_totaltime/60, 2)) + "minutes")
 
+    if args.audio:
+        print("Do you wish to convert your webm audio file?")
+        conversion = input("Press enter to skip or (y) to convert:")
+        if conversion:
+            try:
+                sound = AudioSegment.from_file(str(stream.default_filename))
+                file_handle= sound.export(str(stream.default_filename[:-5])+".mp3", format="mp3")
+                print("Conversion Done!\n")
+            except e:
+                print(e)
 
 parser = argparse.ArgumentParser(description='List & Download available YouTube streams')
 #Main arguments (mutual exclusive)
@@ -43,25 +61,38 @@ group.add_argument('-all', '--all', action="store_true", help="Choose to all get
 
 #Other Arguments
 parser.add_argument('--verbose', action="store_true", help="Choose verbose mode")
-parser.add_argument('videos', nargs="*", help="Video or playlist URL")
+parser.add_argument('url', nargs="*", help="Video or playlist URL")
 args = parser.parse_args()
 
 
-url = str(args.videos)
-youtube = YouTube(url)
 
-print('Avaliable Media Streams:')
-if args.all:
-    for stream in youtube.streams.all():
-        print(stream)
-    choose_download(youtube)
+url = str(args.url)
 
-if args.video:
-    for stream in youtube.streams.filter(only_video=True).all():
-        print(stream)
-    choose_download(youtube)
+# Searches for the video and sets up the callback to run the progress indicator.
+try:
+    youtube = YouTube(url)
+except Exception as e:
+    print("ERROR. Check your:\n  -connection\n  -url is a YouTube url\n\nTry again.")
+    print(e)
 
-if args.audio:
-    for stream in youtube.streams.filter(only_audio=True).all():
-        print(stream)
-    choose_download(youtube)
+
+try:
+    print('Avaliable Media Streams:')
+    if args.all:
+        for stream in youtube.streams.all():
+            print(stream)
+        choose_download(youtube)
+
+    if args.video:
+        for stream in youtube.streams.filter(only_video=True).all():
+            print(stream)
+        choose_download(youtube)
+
+    if args.audio:
+        for stream in youtube.streams.filter(only_audio=True).all():
+            print(stream)
+        choose_download(youtube)
+
+except Exception as e:
+        print("ErrorDownloadVideo  |  " + str(youtube.title) + "-" + str(url))
+        print(e)
